978f82482caf4bef5f9bdaa00c608c4c4196e6a5
Edson Angoti Jr <angoti@gmail.com>
2021-12-21 13:03:45 -0300

    navegador javascript para uma lista de objetos

--- adoteumpet/src/main/java/br/edu/iftm/adoteumpet/repository/AnimalRepository.java
+++ adoteumpet/src/main/java/br/edu/iftm/adoteumpet/repository/AnimalRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
 import br.edu.iftm.adoteumpet.model.Animal;
 import br.edu.iftm.adoteumpet.model.Usuario;
 
@@ -19,31 +20,34 @@ public class AnimalRepository {
         List<Animal> lista = new ArrayList<Animal>();
         lista = jdbc.query(consulta,
                 (res, numeroDaLinha) -> new Animal(res.getInt("id"), res.getString("nome"),
-                        res.getString("data_nasc"), res.getString("sexo").charAt(0),
+                        res.getDate("data_nasc"), res.getString("sexo").charAt(0),
                         res.getString("raca"),
-                        res.getString("porte").charAt(0), res.getString("data_entrada"),
-                        res.getString("descricao"), res.getString("data_adocao"),
-                        res.getBoolean("validacao_adocao")));
-        for (Animal animal : lista) { System.out.println("----------------" + animal.getNome());
+                        res.getString("porte").charAt(0), res.getDate("data_entrada"),
+                        res.getString("descricao"), res.getDate("data_adocao"),
+                        res.getBoolean("validacao_adocao"), new Usuario(), res.getString("imagem")));
+        for (Animal animal : lista) {
+            System.out.println("----------------" + animal.getNome());
             String sql = "SELECT u.id , u.nome, u.cpf, u.email FROM ANIMAL, USUARIO u WHERE u.ID = ANIMAL.ID_USER AND ANIMAL.ID = ?;";
             Usuario usuario = null;
             try {
-                usuario = jdbc.queryForObject( 
-                    sql,
-                    (res, linha) -> new Usuario(
-                            res.getInt("id"),
-                            res.getString("nome"),
-                            res.getString("cpf"),
-                            res.getString("email") /*,
-                            res.getString("endereco"),
-                            res.getString("telefone"),
-                            res.getString("dataNasc"),
-                            (char) res.getInt("sexo"),
-                            res.getString("senha")*/),
-                    animal.getId());
+                usuario = jdbc.queryForObject(
+                        sql,
+                        (res, linha) -> new Usuario(
+                                res.getInt("id"),
+                                res.getString("nome"),
+                                res.getString("cpf"),
+                                res.getString("email") /*
+                                                        * ,
+                                                        * res.getString("endereco"),
+                                                        * res.getString("telefone"),
+                                                        * res.getString("dataNasc"),
+                                                        * (char) res.getInt("sexo"),
+                                                        * res.getString("senha")
+                                                        */),
+                        animal.getId());
             } catch (Exception e) {
                 System.out.println("ERRO ================================== " + e.getMessage());
-            } 
+            }
             animal.setUsuario(usuario);
         }
         return lista;
@@ -62,12 +66,15 @@ public class AnimalRepository {
     }
 
     public Animal buscaPorIdAnimal(Integer id) {
-        return jdbc.queryForObject("select * from animal where id_pet = ?", (resultSet, rowNum) -> {
-            return new Animal(resultSet.getInt("id_pet"), resultSet.getString("nome"), resultSet.getString("data_nasc"), resultSet.getString("sexo").charAt(0), resultSet.getString("raca"),
-                    resultSet.getString("porte").charAt(0), resultSet.getString("data_entrada"),
-                    resultSet.getString("descricao"), resultSet.getString("data_adocao"),
-                    resultSet.getBoolean("validacao_adocao"));
-        }, id);
+        return jdbc.queryForObject(
+                "select * from animal where id_pet = ?",
+                (res, rowNum) -> {
+                    return new Animal(res.getInt("id"), res.getString("nome"), res.getDate("data_nasc"),
+                            res.getString("sexo").charAt(0), res.getString("raca"), res.getString("porte").charAt(0),
+                            res.getDate("data_entrada"), res.getString("descricao"), res.getDate("data_adocao"),
+                            res.getBoolean("validacao_adocao"), new Usuario(), res.getString("imagem"));
+                },
+                id);
     }
 
     public int atualizaAnimal(Animal animal) {
@@ -75,20 +82,19 @@ public class AnimalRepository {
         return jdbc.update(consulta, animal.getNome(), animal.getId());
     }
 
-
-    /* ============================= teste validação adoção ==============================================*/
+    /*
+     * ============================= teste validação adoção
+     * ==============================================
+     */
 
     public List<Animal> buscaAnimaisEmAdocao() {
-        String consulta = "select * from animais where id_user IS NOT NULL;";
-        return jdbc.query(consulta,
-        (res,linha) -> new Animal(res.getInt("id_pet"), res.getString("nome"),
-                        res.getString("data_nasc"), res.getString("sexo").charAt(0),
-                        res.getString("raca"),
-                        res.getString("porte").charAt(0), res.getString("data_entrada"),
-                        res.getString("descricao"), res.getString("data_adocao"),
-                        res.getBoolean("validacao_adocao"),
-                        new Usuario())
-        );
+        String consulta = "select * from animais where id_user IS NULL;";
+        return jdbc.query(
+                consulta,
+                (res, linha) -> new Animal(res.getInt("id_pet"), res.getString("nome"), res.getDate("data_nasc"),
+                        res.getString("sexo").charAt(0), res.getString("raca"), res.getString("porte").charAt(0),
+                        res.getDate("data_entrada"), res.getString("descricao"), res.getDate("data_adocao"),
+                        res.getBoolean("validacao_adocao"), new Usuario(), res.getString("imagem")));
     }
 
     public void atualizaAdocao(Animal animal) {
